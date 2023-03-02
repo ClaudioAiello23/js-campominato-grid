358cfad83342b735ed3cd59469613f616e6af6b8
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-02 18:01:04 +0100

    inizio impostazioni file JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 'use strict';
+/* <div class="cell_box"></div> */
+
+/* 
+*************************
+FUNZIONI
+************************* 
+*/
+// FUNZIONE A
+// Funzione per creare un elemento HTML e agganciarci una classe css
+function createMyElement(element, className) {
+    const element = document.createElement(element);  
+    element.classList.add(className);
+}
+
+/* 
+*************************
+ALTRE ISTRUZIONI
+************************* 
+*/
+
+// Creo una variabile che richiama il contenitore delle celle
+const cellContainer = document.querySelector(".cell_container");
+
+// Sfrutto la funzione A per creare un div che richiama la classe della cella
+createMyElement('div', 'cell_box');
+
+
+
+
+
+
+
