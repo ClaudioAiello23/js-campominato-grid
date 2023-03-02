245f1000af5ecd1ce4f18b890a9421dd411a8dbb
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-02 18:39:20 +0100

    sistemazione ciclo FOR per replicare le celle html

--- js/main.js
+++ js/main.js
@@ -26,13 +26,20 @@ ALTRE ISTRUZIONI
 // Creo una variabile che richiama il contenitore delle celle
 const cellContainer = document.querySelector(".cell_container");
 
-/* Creo una nuova variabile che riguarda l'elemento creato, cui associo
- la "funzione A" (vedi FUNZIONI) per creare un div che richiama la classe della cella*/
-const cellBox = createMyElement('div', 'cell_box');
+
+/* Creo una nuova variabile cellBox che riguarda l'elemento creato, 
+cui associo la "funzione A" (vedi FUNZIONI) per creare un div che
+richiama la classe della cella; appendo la nuova variabile al 
+cellContainer; il tutto lo inserisco in un ciclo FOR per replicare
+100 volte l'operazione e creare la griglia di partenza*/
+for (let i = 1; i <= 100; i++) {
+    let cellBox = createMyElement('div', 'cell_box');
+    cellContainer.append(cellBox);   
+}
 
 
-// Aggancio la cellBox al cellContainer
-cellContainer.append(cellBox);
+
+
 
 
 
