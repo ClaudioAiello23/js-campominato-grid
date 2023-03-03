0d6bba16f7793e6a49d0ed28340984674946f2e3
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-03 07:36:36 +0100

    aggiunta griglia a comparsa con click su Play

--- js/main.js
+++ js/main.js
@@ -18,26 +18,42 @@ function createMyElement(htmlElement, className) {
 }
 
 
+// // FUNZIONE B - PROVA CREAZIONE FUNZIONE AGGIUNGI CLASSE - DA VERIFICARE
+// // Funzione per aggiungere una classe
+// function addClassHtml(oldClass, newClass) {
+//     const element = document.querySelector(oldClass).classList.add(newClass);
+//     return element;
+// }
+
+
 /* 
 *************************
 ALTRE ISTRUZIONI
 ************************* 
 */
 
+
 // Creo una variabile che richiama il contenitore delle celle
 const cellContainer = document.querySelector(".cell_container");
-
 
 /* Creo una nuova variabile cellBox che riguarda l'elemento creato, 
 cui associo la "funzione A" (vedi FUNZIONI) per creare un div che
 richiama la classe della cella; appendo la nuova variabile al 
 cellContainer; il tutto lo inserisco in un ciclo FOR per replicare
-100 volte l'operazione e creare la griglia di partenza*/
+100 volte l'operazione e creare la griglia di partenza; in più chiedo
+di stampare a console il numero della cella quando la clicco*/
 for (let i = 1; i <= 100; i++) {
     let cellBox = createMyElement('div', 'cell_box');
-    cellBox.innerHTML += [i];
-    cellContainer.append(cellBox);   
+    cellBox.innerText = [i];
+    cellContainer.append(cellBox);
+    cellBox.addEventListener('click', function() {
+       console.log(i);
 }
+
+)
+     
+}
+
 
 // Creo una variabile che richiama il contenitore del bottone
 const buttonContainer = document.querySelector(".btn_box");
@@ -47,13 +63,23 @@ const buttonContainer = document.querySelector(".btn_box");
 bottone e aggiungere la relativa classe; aggiungo la scritta PLAY 
 all'elemento e lo appendo al contenitore button box.*/
 const playButton = createMyElement('button', 'play_button');
-playButton.innerHTML += 'PLAY';
+playButton.innerText = 'PLAY';
 buttonContainer.append(playButton);
 
 
+/* Creo una variabile showContainer che si aggancia alla classe
+main_container che contiene l'intera griglia; la griglia è nascosta
+di default al caricamento del sito e si attiva con il click
+sul tasto PLAY; viene aggiunta la classe "show_main_container"*/
+let showContainer = document.querySelector('.main_container');
+playButton.addEventListener('click',
+    
+    function() {
+        showContainer.classList.add('show_main_container');
+      
+}
 
-
-
+)
 
 
 
